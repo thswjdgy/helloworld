81c5ccf3f6a9a7fa734b6f9a5b6b727ac67944b2
Son JungHyo <thswjdgy@g.shingu.ac.kr>
2025-11-22 21:42:26 +0900

    Restore lib directory

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:bamtol_market_app/firebase_options.dart';
+import 'package:bamtol_market_app/app.dart';
+import 'package:bamtol_market_app/common/controller/authentication_controller.dart';
+import 'package:bamtol_market_app/common/controller/data_load_controller.dart';
+import 'package:bamtol_market_app/home/page/home_page.dart';
+import 'package:bamtol_market_app/splash/controller/splash_controller.dart';
+import 'package:bamtol_market_app/user/login/page/login_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+late SharedPreferences prefs;
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  prefs = await SharedPreferences.getInstance();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: '당근마켓 클론코딩',
+      initialRoute: '/',
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          color: Color(0xff212123),
+          titleTextStyle: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        scaffoldBackgroundColor: const Color(0xff212123),
+      ),
+      initialBinding: BindingsBuilder(() {
+        Get.put(SplashController());
+        Get.put(DataLoadController());
+        Get.put(AuthenticationController());
+      }),
+      getPages: [
+        GetPage(name: '/', page: () => const App()),
+        GetPage(name: '/home', page: () => const HomePage()),
+        GetPage(name: '/login', page: () => const LoginPage()),
+      ],
+    );
+  }
+}
